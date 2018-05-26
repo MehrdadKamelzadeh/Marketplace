919b1c500b65b96de350f56e81ec3e10a2802a5c
Mehrdad Kamelzadeh <mehrdad.kamelzadeh@gmail.com>
2018-05-26 21:49:02 +0430

    Raact-Router added

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import ItemsList from '../containers/items_list';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
+const Home = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+    </div>);
+};
 export default class App extends Component {
   render() {
     return (
-      <ItemsList />
+      // <ItemsList />
+      <BrowserRouter>
+        <div>
+          <ul className="nav navbar nav-inline ">
+            <li className="nav-item"><Link to="/">Home</Link></li>
+            <li className="nav-item"> <Link to="/main">Main</Link></li>
+          </ul>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/main" component={ItemsList} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
